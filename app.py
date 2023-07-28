65f57208197f1b148a2547fa52502767852c8d99
Lino Kaiser <87489114+linokaiser@users.noreply.github.com>
2023-07-28 15:46:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask,Response
-from prometheus_client import Gauge,generate_latest
+from flask import Flask, Response
+from prometheus_client import Gauge, generate_latest
 import boto3
 from datetime import datetime, timedelta
 import time, os
@@ -8,86 +8,79 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.interval import IntervalTrigger
 
 QUERY_PERIOD = os.getenv('QUERY_PERIOD', "1800")
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_REGION = os.getenv('AWS_REGION', 'us-west-2')  # Change 'us-west-2' to your desired region
 
 app = Flask(__name__)
 CONTENT_TYPE_LATEST = str('text/plain; version=0.0.4; charset=utf-8')
-client = boto3.client('ce')
+session = boto3.Session(
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION
+)
 
-if os.environ.get('METRIC_TODAY_DAILY_COSTS') is not None:
-    g_cost = Gauge('aws_today_daily_costs', 'Today daily costs from AWS')
-if os.environ.get('METRIC_YESTERDAY_DAILY_COSTS') is not None:
-    g_yesterday = Gauge('aws_yesterday_daily_costs', 'Yesterday daily costs from AWS')
-if os.environ.get('METRIC_TODAY_DAILY_USAGE') is not None:
-    g_usage = Gauge('aws_today_daily_usage', 'Today daily usage from AWS')
-if os.environ.get('METRIC_TODAY_DAILY_USAGE_NORM') is not None:
-    g_usage_norm = Gauge('aws_today_daily_usage_norm', 'Today daily usage normalized from AWS')
+if os.environ.get('METRIC_COST_LAST_MONTH') is not None:
+    g_cost_last_month = Gauge('aws_cost_last_month', 'Cost from AWS for last month')
+
+if os.environ.get('METRIC_COST_THIS_MONTH') is not None:
+    g_cost_this_month = Gauge('aws_cost_this_month', 'Cost from AWS for this month')
+
+if os.environ.get('METRIC_COST_BEFORE_LAST_MONTH') is not None:
+    g_cost_before_last_month = Gauge('aws_cost_before_last_month', 'Cost from AWS for the month before last month')
 
 scheduler = BackgroundScheduler()
 
 def aws_query():
     print("Calculating costs...")
     now = datetime.now()
-    yesterday = datetime.today() - timedelta(days=1)
-    two_days_ago = datetime.today() - timedelta(days=2)
-    if os.environ.get('METRIC_TODAY_DAILY_COSTS') is not None:
+    last_month_start = datetime(now.year, now.month - 1, 1)
+    this_month_start = datetime(now.year, now.month, 1)
+    before_last_month_start = datetime(now.year, now.month - 2, 1)
 
-        r = client.get_cost_and_usage(
+    ce_client = session.client('ce')
+
+    if os.environ.get('METRIC_COST_LAST_MONTH') is not None:
+        r = ce_client.get_cost_and_usage(
             TimePeriod={
-                'Start': yesterday.strftime("%Y-%m-%d"),
-                'End':  now.strftime("%Y-%m-%d")
+                'Start': last_month_start.strftime("%Y-%m-%d"),
+                'End':  this_month_start.strftime("%Y-%m-%d")
             },
-            Granularity="DAILY",
+            Granularity="MONTHLY",
             Metrics=["BlendedCost"]
         )
-        cost = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
-        print("Updated AWS Daily costs: %s" %(cost))
-        g_cost.set(float(cost))
+        cost_last_month = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
+        print("Cost from last month: %s" % cost_last_month)
+        g_cost_last_month.set(float(cost_last_month))
 
-    if os.environ.get('METRIC_YESTERDAY_DAILY_COSTS') is not None:
-        r = client.get_cost_and_usage(
+    if os.environ.get('METRIC_COST_THIS_MONTH') is not None:
+        r = ce_client.get_cost_and_usage(
             TimePeriod={
-                'Start': two_days_ago.strftime("%Y-%m-%d"),
-                'End':  yesterday.strftime("%Y-%m-%d")
+                'Start': this_month_start.strftime("%Y-%m-%d"),
+                'End':  now.strftime("%Y-%m-%d")
             },
-            Granularity="DAILY",
+            Granularity="MONTHLY",
             Metrics=["BlendedCost"]
         )
-        cost_yesterday = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
-        print("Yesterday's AWS Daily costs: %s" %(cost_yesterday))
-        g_yesterday.set(float(cost_yesterday))
+        cost_this_month = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
+        print("Cost for this month: %s" % cost_this_month)
+        g_cost_this_month.set(float(cost_this_month))
 
-
-    if os.environ.get('METRIC_TODAY_DAILY_USAGE') is not None:
-        r = client.get_cost_and_usage(
+    if os.environ.get('METRIC_COST_BEFORE_LAST_MONTH') is not None:
+        r = ce_client.get_cost_and_usage(
             TimePeriod={
-                'Start': yesterday.strftime("%Y-%m-%d"),
-                'End':  now.strftime("%Y-%m-%d")
+                'Start': before_last_month_start.strftime("%Y-%m-%d"),
+                'End':  last_month_start.strftime("%Y-%m-%d")
             },
-            Granularity="DAILY",
-            Metrics=["UsageQuantity"]
+            Granularity="MONTHLY",
+            Metrics=["BlendedCost"]
         )
-        usage = r["ResultsByTime"][0]["Total"]["UsageQuantity"]["Amount"]
-        print("Updated AWS Daily Usage: %s" %(usage))
-        g_usage.set(float(usage))
-
-    if os.environ.get('METRIC_TODAY_DAILY_USAGE_NORM') is not None:
-
-        r = client.get_cost_and_usage(
-            TimePeriod={
-                'Start': yesterday.strftime("%Y-%m-%d"),
-                'End':  now.strftime("%Y-%m-%d")
-            },
-            Granularity="DAILY",
-            Metrics=["NormalizedUsageAmount"]
-        )
-        usage_norm = r["ResultsByTime"][0]["Total"]["NormalizedUsageAmount"]["Amount"]
-        print("Updated AWS Daily Usage Norm: %s" %(usage_norm))
-        g_usage_norm.set(float(usage_norm))
+        cost_before_last_month = r["ResultsByTime"][0]["Total"]["BlendedCost"]["Amount"]
+        print("Cost for the month before last month: %s" % cost_before_last_month)
+        g_cost_before_last_month.set(float(cost_before_last_month))
 
     print("Finished calculating costs")
-
     return 0
-
 
 @app.route('/metrics/')
 def metrics():
